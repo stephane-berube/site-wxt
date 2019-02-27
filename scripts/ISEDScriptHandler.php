fca96b76773bcf095f2cc9183bce9c858577c469
Stephane Berube <stephane.berube@canada.ca>
2019-02-27 07:53:03 -0500

    composer: Add script to edit .htaccess file on install
    The .htaccess comes from drupal-scaffold

---
+++ scripts/ISEDScriptHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ISEDDrupalWxT\WxT;
+
+use Composer\Script\Event;
+use Symfony\Component\Filesystem\Filesystem;
+
+class ISEDScriptHandler {
+
+  /**
+   * Retrieves the Drupal root directory.
+   *
+   * @param string $project_root
+   *   Drupal root directory.
+   */
+  protected static function getDrupalRoot($project_root) {
+    return $project_root . '/html';
+  }
+
+   /**
+   * Deploys front-end related libraries to WxT's install profile directory.
+   *
+   * @param \Composer\Script\Event $event
+   *   The script event.
+   */
+  public static function editHtaccess(Event $event) {
+    $fs = new Filesystem();
+    $root = static::getDrupalRoot(getcwd());
+
+    $cors = <<<'EOD'
+
+<FilesMatch "\.(ttf|otf|eot|woff|woff2)$">
+  <IfModule mod_headers.c>
+    ## Set CORS for ised.canada.ca, api.canada.ca and all of their subdomains
+    ## Based on: https://stackoverflow.com/a/27990162
+    #
+    SetEnvIf Origin ^(https?://(.+\.)?(ised-isde|api)\.canada\.ca(?::\d{1,5})?)$   CORS_ALLOW_ORIGIN=$1
+    Header append Access-Control-Allow-Origin  %{CORS_ALLOW_ORIGIN}e   env=CORS_ALLOW_ORIGIN
+    Header merge  Vary "Origin"
+  </IfModule>
+</FilesMatch>
+EOD;
+
+    // 'Undefined method'
+    // $fs->appendToFile($root . '/.htaccess', $cors);
+
+    file_put_contents($root . '/.htaccess', $cors, FILE_APPEND);
+  }
+}
